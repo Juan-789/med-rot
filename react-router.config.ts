5b2b22c45fe668f0593c7ab6cc86f91cdaab6808
Osasuyi Uhunmwagho <maynosa165@gmail.com>
2025-03-17 15:29:15 -0400

    hmm

--- react-router.config.ts
+++
@@ -1,4 +0,0 @@
-import type { Config } from "@react-router/dev/config";
-import { reactRouterConfigOptions } from "gadget-server/react-router";
-
-export default reactRouterConfigOptions satisfies Config;
--- web/api.ts
+++
@@ -1,5 +0,0 @@
-// Sets up the API client for interacting with your backend.
-// For your API reference, visit: https://docs.gadget.dev/api/lebron
-import { Client } from "@gadget-client/lebron";
-
-export const api = new Client();
